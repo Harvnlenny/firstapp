3fcbfea2f242d6a9b23d4b34d9345f72a279f67b
Harvnlenny <harvnlenny@gmail.com>
2017-01-25 15:34:36 -0500

    examine the Django admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item
+
+#add a class so I can customize interface.
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ['title', 'amount']
+
+#register our model with the admin
+admin.site.register(Item, ItemAdmin)
